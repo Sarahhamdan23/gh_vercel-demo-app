52a8cec9903a7bd3b1ee6d59d2400b10f34d4ddf
Sarah Hamdan <141799454+Sarahhamdan23@users.noreply.github.com>
2024-09-23 18:29:41 -0500

    logout functionality working

--- auth.config.js
+++ auth.config.js
@@ -6,12 +6,12 @@ export const authConfig = {
     callbacks: {
       authorized({ auth, request: { nextUrl } }) {
         const isLoggedIn = !!auth?.user;
-        const isOnDashboard = nextUrl.pathname.startsWith('/ui/dashboard');
+        const isOnDashboard = nextUrl.pathname.startsWith('/ui');
         if (isOnDashboard) {
           if (isLoggedIn) return true;
           return false; // Redirect unauthenticated users to the login page
         } else if (isLoggedIn) {
-          return Response.redirect(new URL('/ui/dashboard', nextUrl));
+          return Response.redirect(new URL('/ui', nextUrl));
         }
         return true;
       },
